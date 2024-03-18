31b2feb68f5a7992162c2516b0025a2833cc9525
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2024-03-18 17:02:10 +0100

    Initial commit

---
+++ BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Zakir Ufuk Sahiner on 18.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Zakir Ufuk Sahiner on 18.03.24.
+//
+// 100 Days of SwiftUI Project 11
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+            Text("Hello, World!")
+        }
+}
+
+#Preview {
+    ContentView()
+}
